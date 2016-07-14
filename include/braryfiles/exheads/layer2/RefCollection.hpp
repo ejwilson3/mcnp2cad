d76c0671ff3c8af373c7db788856445229435b75
ejwilson3 <ejwilson3@wisc.edu>
2016-07-14 13:05:41 -0500

    Cleaned up second layer of dependencies; CGMApp.hpp is copyright protected but belongs in layer2.

--- include/braryfiles/exheads/layer2/RefCollection.hpp
+++ include/braryfiles/exheads/layer2/RefCollection.hpp
@@ -3,26 +3,7 @@
 
 #include "RefEntity.hpp"
 
-class CollectionEntity;
-
-class CUBIT_GEOM_EXPORT RefCollection : public RefEntity
-{
-   public:
-      RefCollection();
-      virtual ~RefCollection();
-      CollectionEntity* get_collection_entity_ptr(){ return collectionEntityPtr; }
-      CollectionEntity const* get_collection_entity_ptr() const { return collectionEntityPtr; }
-      //R CollectionEntity*
-      //R- A pointer to a colleciton entity associated with the grouping entity.
-      //- This function returns a pointer to the collection entity associated 
-      //- with this object.
-      
-      CubitStatus set_collection_entity_ptr(CollectionEntity* collection_entity_ptr);
-      
-  protected:
-     CollectionEntity* collectionEntityPtr;
-     
-};
+class CUBIT_GEOM_EXPORT RefCollection : public RefEntity{};
 
 #endif
 
