8e10416d54ed4d543ef3b8910d2a222ea62ee82e
ejwilson3 <ejwilson3@wisc.edu>
2016-07-18 11:50:38 -0500

    clean up through the third layer of dependencies.

--- include/braryfiles/exheads/layer2/layer3/CubitAttribManager.hpp
+++ include/braryfiles/exheads/layer2/layer3/CubitAttribManager.hpp
@@ -3,12 +3,9 @@
 
 class RefEntity;
 class CubitAttrib;
-class CubitAttribUser;
 class CubitSimpleAttrib;
 
-#include "CubitDefines.h"
 #include "DLIList.hpp"
-#include "CubitString.hpp"
 #include "CubitGeomConfigure.h"
 
 #if defined(WIN32)
@@ -23,8 +20,6 @@ typedef CubitAttrib* (*CACreateFunction)(RefEntity*, CubitSimpleAttrib*);
 class CUBIT_GEOM_EXPORT CubitAttribManager
 {
 public:
-  CubitAttribManager();
-  virtual ~CubitAttribManager();
 
   // stores factory function and settings for an attribute
   // returns a key that can be used to refer to the attribute type to avoid search by name
@@ -53,106 +48,7 @@ public:
 
   void get_registered_types(DLIList<int> &types);
 
-  CubitAttrib *create_cubit_attrib(const int attrib_type,
-                                   RefEntity *attrib_user,
-                                   CubitSimpleAttrib *p_csa);
-
-  const char * att_internal_name(int attrib_type);
-
-  CubitBoolean auto_actuate_flag(int attrib_type);
-  CubitStatus set_auto_actuate_flag(int attrib_type, CubitBoolean value);
-  void set_all_auto_actuate_flags(CubitBoolean value);
-
-  CubitBoolean auto_update_flag(int attrib_type);
-  CubitStatus set_auto_update_flag(int attrib_type, CubitBoolean value);
-  void set_all_auto_update_flags(CubitBoolean value);
-
-  CubitBoolean auto_write_flag(int attrib_type);
-  CubitStatus set_auto_write_flag(int attrib_type, CubitBoolean value);
-  void set_all_auto_write_flags(CubitBoolean value);
-
-  CubitBoolean auto_read_flag(int attrib_type);
-  CubitStatus set_auto_read_flag(int attrib_type, CubitBoolean value);
-  void set_all_auto_read_flags(CubitBoolean value);
-
-  CubitBoolean actuate_in_constructor(int attrib_type);
-  CubitBoolean actuate_after_geom_changes(int attrib_type);
-
-  CubitStatus auto_update_attribs(RefEntity *cau);
-
-//  CubitStatus actuate_list(DLIList<CubitAttribUser*> entity_list);
-
-  int attrib_type(CubitSimpleAttrib *csa_ptr);
-  int attrib_type(const char* name);
-  int attrib_type_from_internal_name(const char* name);
-
-//  void clear_attrib_importeds() ;
-//  void report_attrib_importeds();
-
-  int auto_flag();
   void auto_flag(int flag);
-
-  bool silent_flag() {return silentFlag;}
-  void silent_flag(const bool flag) {silentFlag = flag;}
-
-  const char * att_name(int attrib_type);
-
-private:
-/*
-  class CARegisterData
-  {
-  public:
-    CARegisterData(const char *type_name, const char *internal_name,
-                   CACreateFunction p_func, unsigned int flags)
-    {
-      mTypeName = type_name;
-      mInternalName = internal_name;
-      mCreateFunction = p_func;
-      mFlags = flags;
-    }
-
-    ~CARegisterData();
-
-    CubitString mTypeName;
-    CubitString mInternalName;
-    CACreateFunction mCreateFunction;
-    unsigned int mFlags;
-  };
-
-  #define CA_AUTOACTUATE             0x01
-  #define CA_AUTOUPDATE              0x02
-  #define CA_AUTOWRITE               0x04
-  #define CA_AUTOREAD                0x08
-  #define CA_ACTUATEINCONSTRUCTOR    0x10
-  #define CA_ACTUATEAFTERGEOMCHANGES 0x20
-
-  std::map<int, CARegisterData*> mTypeToCAData;
-//struct ltstr
-//{
-//  bool operator()(const char* s1, const char* s2) const
-//  {
-//    return strcmp(s1, s2) < 0;
-//  }
-//};
-//  std::map<const char*, int, ltstr> mTypeNameToType;
-//  std::map<const char*, int, ltstr> mInternalNameToType;
-*/
-
-  DLIList<int> mTypes;
-  DLIList<char*> mTypeNames;
-  DLIList<char*> mInternalNames;
-  DLIList<CACreateFunction> mCreatorFunctions;
-  DLIList<CubitBoolean> mAutoActuateFlags;
-  DLIList<CubitBoolean> mAutoUpdateFlags;
-  DLIList<CubitBoolean> mAutoWriteFlags;
-  DLIList<CubitBoolean> mAutoReadFlags;
-  DLIList<CubitBoolean> mActuateInConstructor;
-  DLIList<CubitBoolean> mActuateAfterGeomChanges;
-
-  // TODO - get rid of mAttribImported
-//  DLIList<CubitBoolean> mAttribImported;
-
-  bool silentFlag;
 
 };
 
--- include/braryfiles/exheads/layer2/layer3/TopologyEntity.hpp
+++ include/braryfiles/exheads/layer2/layer3/TopologyEntity.hpp
@@ -25,82 +25,17 @@
 #include "ModelEntity.hpp"
 #include "BridgeManager.hpp"
 
-#include "CubitEventDefines.h"
 // ********** END CUBIT INCLUDES           **********
-
-// ********** BEGIN FORWARD DECLARATIONS   **********
-class RefEntity ;
-template <class X> class DLIList ;
-class RefVolume ;
-class RefFace ;
-class RefEdge ;
-class RefVertex ;
-class Shell;
-class Loop;
-class Chain;
-class CoVolume;
-class CoFace;
-class CoEdge;
-class CoVertex;
-class GeometryQueryEngine ;
-class RefVertex;
-class RefEdge;
-class RefFace;
-class RefVolume;
-class Body;
-class RefGroup;
-
-// ********** END FORWARD DECLARATIONS     **********
-
-// ********** BEGIN MACROS DEFINITIONS     **********
-// ********** END MACROS DEFINITIONS       **********
 
 class CUBIT_GEOM_EXPORT TopologyEntity: public ModelEntity
 {
 public :
-  
-  TopologyEntity() ;
-    //- The default constructor
-  
-  virtual ~TopologyEntity();
-    //- A pure virtual destructor - this ensures that the class is
-    //- non-instantiable.
 
   static const char* get_class_name()
      { return "TopologyEntity"; }
   
-  virtual const char* class_name() const
-     { return get_class_name(); }
-  
-  GeometryQueryEngine* get_geometry_query_engine() const;
-    //R GeometryQueryEngine*
-    //R- A pointer to the geometry query engine associated with
-    //R- the object.
-    //- This function returns a pointer to the geometry query engine
-    //- associated with the object.  If this TE has been merged, the
-    //- merged TEs may have different GQEs.  In that case, the GQE used
-    //- for evaluations is returned.
-  
-  CubitBoolean is_directly_related( TopologyEntity* entity );
-    //- Return TRUE if entity is this or a direct parent or child of this.
-  
-  CubitStatus bodies(DLIList<Body*>& body_list);
-  CubitStatus shells(DLIList<Shell*>& shell_list);
-  CubitStatus loops( DLIList<Loop*>& loop_list );
-  CubitStatus chains(DLIList<Chain*>& chain_list);
   CubitStatus ref_volumes(DLIList<RefVolume*>& ref_volume_list);
-  CubitStatus ref_faces(DLIList<RefFace*>& ref_face_list);
-  CubitStatus ref_edges(DLIList<RefEdge*>& ref_edge_list);
-  CubitStatus ref_vertices(DLIList<RefVertex*>& ref_vertex_list);
-  CubitStatus co_volumes(DLIList<CoVolume*>& co_volume_list);
-  CubitStatus co_faces(DLIList<CoFace*>& co_face_list);
-  CubitStatus co_edges(DLIList<CoEdge*>& co_edge_list);
-  CubitStatus co_vertices(DLIList<CoVertex*>& co_vertex_list);
 
-  RefVertex *ref_vertex();
-  RefEdge *ref_edge();
-  RefFace *ref_face();
-  RefVolume *ref_volume();
   Body *body();
     //R CubitStatus
     //R- CUBIT_SUCCESS/FAILURE
@@ -119,52 +54,15 @@ public :
     //- the requested list - e.g., if ref_edges is called on a RefEdge
     //- object. 
     //-
+    //this
 
-  CoEdge *co_edge();
-  int num_loops();
-  int num_ref_volumes();
   int num_ref_faces();
   int num_ref_edges();
   int num_ref_vertices();
 
-  const BridgeManager* bridge_manager() const
-    { return &bridgeMan; }
-  BridgeManager* bridge_manager()
-    { return &bridgeMan; }
-/*  
-  virtual EntityType get_topology_bridge_type() const
-    { return TopologyBridge_TYPE; }
-    // Returns the generic TB type associated with this TE.
-*/  
-  virtual void reverse_topology() {}
-    // This method is used by owned BridgeManagers to
-    // notify their owning TopologyEntity when the sense
-    // of the geometry has changed (due to a different
-    // TopologyBridge being used as the primary rep.)
-
-protected:
-
-  CubitStatus set_topology_bridge(TopologyBridge* TB_ptr);
-    // Sets the TB for this TE to be TB_ptr ONLY (removes other TBs).
-  
 private:
   BridgeManager bridgeMan;
   
-  TopologyEntity( const TopologyEntity& );
-  void operator=( const TopologyEntity& );
 };
-
-// ********** BEGIN INLINE FUNCTIONS       **********
-// ********** END INLINE FUNCTIONS         **********
- 
-// ********** BEGIN FRIEND FUNCTIONS       **********
-// ********** END FRIEND FUNCTIONS         **********
- 
-// ********** BEGIN EXTERN FUNCTIONS       **********
-// ********** END EXTERN FUNCTIONS         **********
- 
-// ********** BEGIN HELPER CLASSES         **********
-// ********** END   HELPER CLASSES         **********
-
 #endif
 
